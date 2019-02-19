4990d4a38e34bb80962f50bf1096e13926192fc3
Bach Xuan Nguyen-Ngo <Bach.X.NguyenNgo-1@ou.edu>
2019-02-19 13:25:40 -0600

    Wow

---
+++ src/Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-18
+OU ID_______________: 113466120
+OU EMAIL____________: Bach.X.NguyenNgo-1@ou.edu
+
+Tasks:
+1. Complete Calculator.java Class
+2. Complete Calculator.test Class
+
+Estimated Time (minutes):
+1. 30
+2. 20
+
+Actual Time (minutes):
+1. 45
+2. 30
+*/}
